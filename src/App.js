f06e6de1998558ae698a2061eb12a498c9086e31
Sekharsom <154993718+Sekharsom@users.noreply.github.com>
2024-03-27 11:33:04 +0530

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [count, setCount] = useState(0);
+  const [pause, setPause] = useState(true); 
+  const interval = useRef(null);
+
+  // useEffect(() => {
+  //   console.log("Mounted");
+  //   startTimer();
+  //   stopTimer();
+  //   return () => {
+  //     if(interval.count) {
+  //       clearInterval(interval.count)
+  //     }
+  //   }
+
+
+  // }, [])
+
+  const startTimer = () => {
+    interval.current = setInterval(()=>{
+      setCount((count) => count+1)
+    }, 1000)
+    if (pause){
+      setPause(false)
+  }
+    
+    }
+    
+
+  const stopTimer = () => {
+    clearInterval(interval.current);
+    setPause(true);
+  }
+
+  const resetTimer = () => {
+    clearInterval(interval.current);
+    setPause(true);
+    setCount(0);
+  }
+
+
+
+
+
+
+  return(
+    <div className='header'>
+      <h1>Stop Watch</h1>
+      <div className='timer'>{count}</div>
+      <button onClick={pause ? startTimer : stopTimer}>{pause ? "start" : "stop"}</button>
+      <button onClick={resetTimer}>Reset</button>
     </div>
-  );
+  )
+
 }
+
 
 export default App;
